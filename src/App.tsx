8f21ab2b27de004bd573f9f78ec53fa0fe3fd2e2
Igor Dejak <igordejak@MacBook-Pro.local>
2024-06-19 18:24:10 +0200

    logo for header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 
 import './App.css'
+import Header from './Header'
 
 function App() {
   
 
   return (
-    <>
-      
+    <div>
+            <Header />
       <h1>Test app to track expences and paying</h1>
-
-    </>
+</div>
   )
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,22 @@
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <a href="/">
+          <img src="./img/outtrack.svg" alt="Logo" />
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            Home
+          </li>
+         
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
