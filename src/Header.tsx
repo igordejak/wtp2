c66d5d10b413d3e663a79afec61b19eabbde6971
Tak1s <dkornienko604@gmail.com>
2024-06-19 19:47:58 +0200

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,20 +1,34 @@
+import { useState } from 'react';
 import Link from './Link';
+import Navigation from './Navigation';
 
 const Header = () => {
+  const [activLink, setActivLink] = useState('home');
+  const func = (newActiveLink) => setActivLink(newActiveLink);
+  
   return (
     <header>
-      <div className="logo">
+{/*       <div className="logo">
         <Link href="/">
           <img src="./img/outtrack.svg" alt="Logo" />
         </Link>
-      </div>
-      <nav>
-        <ul>
-          <li>
+      </div> */}
+
+      <Logo />
+      <Navigation place={'header'} />
+      
+{/*       <nav className="sdfdsf">
+        <ul className="ul">
+          <li onClick={func} className="li">
             Home
           </li>
-         
-        </ul>
+         <li>
+            Solutions
+          </li>
+          <li>
+            About
+          </li>
+        </ul> */}
       </nav>
     </header>
   );
