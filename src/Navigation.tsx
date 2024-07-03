23a96198b1e6c88e07b489343e4c1669c1e99a5c
Igor Dejak <igordejak@MacBook-Pro.local>
2024-07-03 18:46:05 +0200

    menu icon functionality

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import Link from "./Link";
 
-const Navigation = () => (
-  <nav className="menu-wrapper">
-    <button id="menu-icon">
-    </button>
-    <ul className="ul">
-      <li className="li">
-        <Link href="/">Home</Link>
-      </li>
-      <li>Solutions</li>
-      <li>About</li>
-    </ul>
-  </nav>
-);
+const Navigation = () => {
+  const [isChange, setIsChange] = useState(true);
+  const handleChange = () => setIsChange(!isChange);
+  return (
+    <nav className="menu-wrapper">
+      <button
+        className={isChange ? "menu-icon" : "menu-icon-hidden"}
+        onClick={handleChange}
+      ></button>
+      <button
+        className={isChange ? "menu-icon-close-hidden" : "menu-icon-close"}
+        onClick={handleChange}
+      ></button>
+      <ul className={isChange ? "menu-block-hidden" : "menu-block"}>
+        <li className="li">
+          <Link href="/">Home</Link>
+        </li>
+        <li>Solutions</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
